951bc7658b87b66cb29823168d9631d7de76a929
Andrey Babaryka <BabarykaSES@yandex.ru>
2019-10-18 20:40:33 +0300

    Второе задание сделал

--- js/complicate.js
+++ js/complicate.js
@@ -1,4 +1,10 @@
 let num = 266219;
-var digits = num.toString().split('');
-var realDigits = digits.map(Number);
-console.log(realDigits);
+let digits = num.toString().split('');
+let realDigits = digits.map(Number);
+let numbers = realDigits[0] * realDigits[1] * realDigits[2] * realDigits[3] * 
+    realDigits[4] *realDigits[5];
+console.log('умножение цифр числа "266219" = ' + numbers);
+
+let extent = numbers ** 3;
+alert('Первые две цыфры: ' + String(extent).slice(0,2));
+//console.log(String(extent).slice(0,2));
