5871ee75a4c2d026af7bb8ae87e8ea525600a338
Andrey Babaryka <BabarykaSES@yandex.ru>
2019-10-18 13:27:40 +0300

    Выполнил обязательное задание

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
 let money = 120000;
-let income = 50000;
-let addExspenses = '1000, 5000, 35000';
+let income = '50000';
+let addExspenses = 'Ноутбук, Квартира, Машина';
 let deposit = true;
 let mission = 1800000;
-let period = 5;
+let period = 12;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+console.log(income.length);
+
+console.log('За период ' + period + ' месяцев, цель заработать ' + mission + ' рублей.');
+
+console.log(addExspenses.toLowerCase());
+console.log(addExspenses.split(','));
+
+let budgetDay = money/30;
+console.log('Дневной бюджет: ' + budgetDay + ',' + ' остаток: ' +  money%30 + ' от деления' );
