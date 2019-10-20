2316b29092740391e3ef085278f9917a8eef5040
Andrey Babaryka <BabarykaSES@yandex.ru>
2019-10-20 12:05:09 +0300

    Внесены правки

--- js/complicate.js
+++ js/complicate.js
@@ -1,10 +1,10 @@
-let num = 266219;
-let digits = num.toString().split('');
-let realDigits = digits.map(Number);
-let numbers = realDigits[0] * realDigits[1] * realDigits[2] * realDigits[3] * 
-    realDigits[4] *realDigits[5];
+let num = 266219,
+    digits = num.toString().split(''),
+    realDigits = digits.map(Number),
+    numbers = realDigits[0] * realDigits[1] * realDigits[2] * realDigits[3] * 
+            realDigits[4] *realDigits[5];
 console.log('умножение цифр числа "266219" = ' + numbers);
 
 let extent = numbers ** 3;
-alert('Первые две цыфры: ' + String(extent).slice(0,2));
-//console.log(String(extent).slice(0,2));
+alert('Первые две цифры: ' + String(extent).slice(0,2));
+
